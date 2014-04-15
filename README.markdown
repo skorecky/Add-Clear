784c5f24e0f6d3c8e3f377277af3ec6713d8a45e
Stephen Korecky <skorecky@gmail.com>
2014-04-15 15:16:04 -0500

    removing image, bumping to 2.0, code cleanup, removing image relience

--- README.markdown
+++ README.markdown
@@ -5,23 +5,28 @@
 
 ## About
 
-**Version 1.1**
+**Version 2.0.0b**
 
-Add Clear is a jQuery plugin that adds a input clearing button on any input you apply it to. It clears the value, and returns focus to that field.
+Add Clear is a jQuery plugin that adds a input clearing button on any input you
+apply it to. It clears the value, and returns focus to that field.
+
+### New in 2.0.0
+- Code Cleanup
+- Removed the clear image since most devices are going beyond 72PPI
 
 ## How to use
-    
+
 - Load jQuery into your project
 - Load Add Clear plugin into your project
 - Setup which elements you would like to apply this plugin to.
 
 ### Usage
-
+```javascript
     <script type="text/javascript" charset="utf-8">
   	  $(document).ready(function(){
   	    $("input").addClear();
   	  });
-  	  
+
   	  // Example onClear option usage
   	  $("input").addClear({
         onClear: function(){
@@ -29,7 +34,7 @@ Add Clear is a jQuery plugin that adds a input clearing button on any input you 
         }
       });
   	</script>
-  	
+```
 ### Available Options
 
 <table>
@@ -39,13 +44,13 @@ Add Clear is a jQuery plugin that adds a input clearing button on any input you 
     <th>Type</th>
   </tr>
   <tr>
-    <td>closeImage</td>
-    <td>clear.png</td>
+    <td>closeSymbol</td>
+    <td>&#10006;</td>
     <td>string</td>
   </tr>
   <tr>
     <td>top</td>
-    <td>0</td>
+    <td>1</td>
     <td>number</td>
   </tr>
   <tr>
@@ -71,4 +76,6 @@ Add Clear is a jQuery plugin that adds a input clearing button on any input you 
 </table>
 
 #### Using Options
-    $("input").addClear({closeImage : "/image/path/close.png",top : -2, right : 6});
+```javascript
+$("input").addClear({top : -2, right : 6});
+```
--- addclear.js
+++ addclear.js
@@ -2,56 +2,56 @@
 // Website: http://stephenkorecky.com
 // Plugin Website: http://github.com/skorecky/Add-Clear
 
-(function($){  
-	$.fn.extend({   
-		addClear: function(options) {  
-			var options =  $.extend({  
-				closeImage: "clear.png",
-				top: 0,
-				right: 4,
-				returnFocus: true,
-				showOnLoad: false,
-				onClear: null
-				}, options);
+(function($){
+  $.fn.extend({
+    addClear: function(options) {
+      var options =  $.extend({
+        closeSymbol: "&#10006;",
+        color: "#CCC",
+        top: 1,
+        right: 4,
+        returnFocus: true,
+        showOnLoad: false,
+        onClear: null
+      }, options);
 
-				$(this).wrap("<span style='position:relative;' class='add-clear-span'>");
-				$(this).after("<a href='#clear'>clear</a>");
-				$("a[href='#clear']").css({
-					'background' : 'transparent url('+options.closeImage+') 0 0 no-repeat', 
-					'display' : 'none',
-					'height' : '16px',
-					'overflow' : 'hidden',
-					'position' : 'absolute',
-					'right' : options.right+'px',
-					'text-indent' : '-9999px',
-					'top' : options.top+'px',
-					'width' : '16px'
-					}, this);
-					
-					if($(this).val().length >= 1 && options.showOnLoad === true) {
-						$(this).siblings("a[href='#clear']").show();
-					}
-					
-					$(this).keyup(function() {
-						if($(this).val().length >= 1) {
-							$(this).siblings("a[href='#clear']").show();
-						} else {
-							$(this).siblings("a[href='#clear']").hide();
-						}
-					});
+      $(this).wrap("<span style='position:relative;' class='add-clear-span'>");
+      $(this).after("<a href='#clear'>"+options.closeSymbol+"</a>");
+      $("a[href='#clear']").css({
+        color: options.color,
+        'text-decoration': 'none',
+        display: 'none',
+        'line-height': 1,
+        overflow: 'hidden',
+        position: 'absolute',
+        right: options.right,
+        top: options.top,
+      }, this);
 
-					$("a[href='#clear']").click(function(){
-						$(this).siblings("input").val("");
-						$(this).hide();
-						if(options.returnFocus === true){
-							$(this).siblings("input").focus();
-						}
-						if (options.onClear){
-              options.onClear($(this).siblings("input"));
-            }
-						return false;
-					});
-					return this; 
-				}  
-			});  
-			})(jQuery);
+      if($(this).val().length >= 1 && options.showOnLoad === true) {
+        $(this).siblings("a[href='#clear']").show();
+      }
+
+      $(this).keyup(function() {
+        if($(this).val().length >= 1) {
+          $(this).siblings("a[href='#clear']").show();
+        } else {
+          $(this).siblings("a[href='#clear']").hide();
+        }
+      });
+
+      $("a[href='#clear']").click(function(){
+        $(this).siblings("input").val("");
+        $(this).hide();
+        if(options.returnFocus === true){
+          $(this).siblings("input").focus();
+        }
+        if (options.onClear){
+          options.onClear($(this).siblings("input"));
+        }
+        return false;
+      });
+      return this;
+    }
+  });
+})(jQuery);
